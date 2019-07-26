b99992e1b7ea3d4dc63eeaa5f25ec66e0bba5c3e
mugi05 <53095150+mugi05@users.noreply.github.com>
2019-07-26 07:33:28 +0530

    Add files via upload

---
+++ +ve & -ve.py
@@ -0,0 +1,15 @@
+x=-1
+if(x>0):
+    print("positive")
+elif(x<0):
+    print("negative")
+else :
+    print("zero")
+x=0
+if(x>0):
+    print("positive")
+elif(x<0):
+    print("negative")
+else :
+    print("zero")
+
